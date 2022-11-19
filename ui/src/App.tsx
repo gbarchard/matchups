08d5825909f03297c84873c8c21ff8c30537e246
Grant Barchard <grant.barchard@gmail.com>
2022-11-19 14:26:22 -0500

    react bootstrap

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,12 @@
+import Button from "react-bootstrap/Button";
+
 function App() {
   return (
-    <div className="text-green-500">
-      My app
+    <div>
+      <Button variant="primary">got here</Button>
+      <div className="text-green-600">got here</div>
     </div>
-  );
+  )
 }
 
 export default App;
