af9fc82b41d5dae2adc0558473e8b96494e1858a
Grant Barchard <grant.barchard@gmail.com>
2022-11-20 13:00:57 -0500

    start script

--- api/app.ts
+++ api/app.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = 3001
 
 app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`)
+  console.log(`Node api is running on port ${port}.`)
 })
 
 app.get("/got-here", gotHere)
---
+++ start.sh
@@ -0,0 +1,1 @@
+cd api && npm run serve & cd ui && npm start
