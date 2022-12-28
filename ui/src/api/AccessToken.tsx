879aee1e0c693bada040d0b8c2d01da2b6aae635
Grant Barchard <grant.barchard@gmail.com>
2022-12-28 18:57:39 -0500

    prevent console errors when logged out

--- ui/src/api/AccessToken.tsx
+++ ui/src/api/AccessToken.tsx
@@ -2,16 +2,17 @@ import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect, useState } from "react"
 
 export function useAccessToken() {
-  const { getAccessTokenSilently } = useAuth0()
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0()
   const [token, setToken] = useState<string>()
 
   useEffect(() => {
+    if (!isAuthenticated) return
     getAccessTokenSilently({
       audience: `https://${process.env.REACT_APP_AUTH_DOMAIN}/api/v2/`,
     }).then((tok) => {
       setToken(tok)
     })
-  }, [getAccessTokenSilently])
+  }, [isAuthenticated, getAccessTokenSilently])
 
   return token
 }
