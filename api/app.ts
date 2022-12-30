92778658c1f85a75febdd72f7a96af4a3d8f66c6
Les Barchard <les.barchard@gmail.com>
2022-12-29 23:30:05 -0500

    Added JWT middleware unless a route is whitelisted

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,11 @@
 import cors from "cors"
 import * as dotenv from "dotenv"
-import express from "express"
+import express, {
+  type Handler,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express"
 import { auth } from "express-oauth2-jwt-bearer"
 import {
   gotHere,
@@ -30,9 +35,31 @@ const checkJwt = auth({
   issuerBaseURL: `${process.env.AUTH_DOMAIN}.us.auth0.com`,
 })
 
-app.get("/api/got-here", checkJwt, gotHere)
+/**
+ * Adds middleware unless a route is included in the whitelist
+ * @param whiteList The list of routes that do not required the middleware
+ * @param middleware The type of middleware to apply to all routes in the whitelist
+ * @returns An Express Handler that either includes the middelware or not depending on if the route is in the whitelist
+ */
+function unless(whiteList: string[], middleware: Handler): Handler {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const pathCheck = whiteList.some((path) => path === req.path)
+    pathCheck ? next() : middleware(req, res, next)
+  }
+}
+
+const JWT_WHITE_LIST = [
+  "/api/characters",
+  "/api/character-content",
+  "/api/matchup-content",
+  "/api/total-scores",
+]
+
+app.use(unless(JWT_WHITE_LIST, checkJwt))
+
+app.get("/api/got-here", gotHere)
 app.get("/api/characters", characters)
-app.post("/api/update-vote", checkJwt, setVote)
+app.post("/api/update-vote", setVote)
 app.post("/api/matchup-content", getMatchupContent)
 app.post("/api/character-content", getCharacterContent)
 app.post("/api/total-scores", getTotalScores)
